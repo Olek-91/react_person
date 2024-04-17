b6567c8010500e416869ffaeeef82e7efc455740
Oleh Dynys <dynys.oleh@gmail.com>
2024-04-17 16:44:46 +0300

    removed variable marriage, add func getMarriageStatus

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,20 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  let notMarriage = `I am not married`;
 
-  if (isMarried) {
-    notMarriage = `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`;
-  }
+  const getMerriageStatus = () => {
+    if (isMarried) {
+      return `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`;
+    }
+
+    return `I am not married`;
+  };
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
-      <p className="Person__partner">{notMarriage}</p>
+      <p className="Person__partner">{getMerriageStatus()}</p>
     </section>
   );
 };
